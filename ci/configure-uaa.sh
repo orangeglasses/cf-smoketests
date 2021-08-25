8f9cf37beef2120a4f96d3de450d744ecb697ee4
Olivier Bootsma <obootsma@jumpbox01.cp.haas.politie>
2021-08-25 10:29:48 +0200

    Added HaaS specific content

--- ci/configure-uaa.sh
+++ ci/configure-uaa.sh
@@ -10,11 +10,11 @@ fi
 uaac token client get $uaaadminuser -s "$uaaadminpassword"
 
 # Set authorized grant types.
-uaac client update $uaaclientid --authorized_grant_types "authorization_code,refresh_token,client_credentials,password" -z itq
+uaac client update $uaaclientid --authorized_grant_types "authorization_code,refresh_token,client_credentials,password" 
 
 # Set authorities for the UAA client (it should be allowed to create a temporary user).
-uaac client update $uaaclientid --authorities "scim.write,scim.read,uaa.resource" -z itq
+uaac client update $uaaclientid --authorities "scim.write,scim.read,uaa.resource"
 
 # Set allowed scopes for UAA client (user should have smoketest.extinguish scope).
-uaac client update $uaaclientid --scope "uaa.user,smoketest.extinguish" -z itq
+uaac client update $uaaclientid --scope "uaa.user,smoketest.extinguish"
 
