1aab84712279512a8e8b994607a13055782749cf
Raymond Voogt <raymond.voogt@politie.nl>
2021-11-02 10:05:18 +0100

    Update clean-up.sh

--- ci/clean-up.sh
+++ ci/clean-up.sh
@@ -8,4 +8,10 @@ export CF_TRACE=./trace.log
 
 # Main function
 
+if $insecure; then
+cf login -a $api -u $username -p $password -o $organization -s $space --skip-ssl-validation
+else
+cf login -a $api -u $username -p $password -o $organization -s $space
+fi
+
 cf curl -v -X DELETE /v2/blobstores/buildpack_cache
